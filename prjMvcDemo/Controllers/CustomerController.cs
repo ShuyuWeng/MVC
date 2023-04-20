8af5a0113d811988bac4e13399bef3fc182ad85a
xiaoyu <wingyjadeweng@gmail.com>
2023-04-20 12:06:58 +0800

    二元一次方程式加入專案檔案。

---
+++ prjMvcDemo/Controllers/CustomerController.cs
@@ -0,0 +1,25 @@
+﻿using prjMvcDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjMvcDemo.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult List()
+        {
+            List<CCustomer> datas = (new CCustomerFactory()).queryAll();
+            return View(datas);
+        }
+
+        public ActionResult create()
+        {
+            return View();
+        }
+    }
+}
